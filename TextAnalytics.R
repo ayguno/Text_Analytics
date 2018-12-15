dea7bf33d3ee32d221745c08aff0e094c830233a
ayguno <ozan@broadinstitute.org>
2018-12-14 22:31:06 -0500

    Initializing

---
+++ TextAnalytics.R
@@ -0,0 +1,5 @@
+############################################
+# Text Analytics (learning from D.Langer)
+# Mainly following playlist at : https://www.youtube.com/watch?v=4vuw0AsHeGw&list=PL8eNk_zTBST8olxIRFoo0YeXxEOkYdoxi
+############################################
+
