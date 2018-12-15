1cdbdea95056c2fe3a1a2e641860b8be20452c98
ayguno <20823263+ayguno@users.noreply.github.com>
2018-12-15 16:53:41 -0500

    Explore data

--- TextAnalytics.R
+++ TextAnalytics.R
@@ -4,3 +4,29 @@
 ############################################
 
 install.packages(c("quanteda","irlba"))
+
+spam.raw <- read.csv(file = 'spam.csv' , stringsAsFactors =  FALSE)
+spam.raw <- spam.raw[,1:2]
+
+names(spam.raw) <- c("Label","Text")
+
+length(which(!complete.cases(spam.raw)))
+
+# It is useful to convert class labels to factors early on
+spam.raw$Label <- factor(spam.raw$Label)
+
+
+# Prop table express them as fractions
+prop.table(table(spam.raw$Label))
+table(spam.raw$Label)
+
+# Let's look at the relative lengths of texts
+
+spam.raw$TextLength <- nchar(spam.raw$Text)
+summary(spam.raw$TextLength)
+
+# This turns out to be a great feature for classification
+library(ggplot2)
+ggplot(spam.raw,aes(x = TextLength, fill = Label))+
+    geom_histogram(binwidth = 5)+
+    theme_bw()
