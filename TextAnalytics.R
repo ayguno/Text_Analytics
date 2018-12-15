c4432522c54d4117b1da632c6242379df782de12
ayguno <ozan@broadinstitute.org>
2018-12-14 22:44:53 -0500

    Error Message to Follow Up

--- TextAnalytics.R
+++ TextAnalytics.R
@@ -3,3 +3,5 @@
 # Mainly following playlist at : https://www.youtube.com/watch?v=4vuw0AsHeGw&list=PL8eNk_zTBST8olxIRFoo0YeXxEOkYdoxi
 ############################################
 
+install.packages(c("quanteda","irlba"))
+# ERROR: dependency ‘RSpectra’ is not available for package ‘quanteda’
